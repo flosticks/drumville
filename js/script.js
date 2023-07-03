82bfa4d93ead66077176fe1f4f455b5ee6a1c576
flosticks <138531359+flosticks@users.noreply.github.com>
2023-07-03 23:06:10 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/**********************************
+    Menu Bar
+***********************************/
+
+var menuBtn =document.getElementById("menuBtn");
+var sideNav =document.getElementById("sideNav");
+
+sideNav.style.right="-250px";
+menuBtn.onclick = function (){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right ="0";
+    }
+    else{
+        sideNav.style.right ="-250px"
+    }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 2000,
+	speedAsDuration: true
+});
